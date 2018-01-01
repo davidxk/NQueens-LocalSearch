45eab6f0f6f85824568210eed5b6afd58d853a39
Zhao Tianyu <davidxk@qq.com>
2018-01-01 11:49:04 +0800

    Minor change

--- min_conflicts.py
+++ min_conflicts.py
@@ -6,9 +6,10 @@ def min_conflicts_value(csp, X, assignment):
     random.shuffle(csp.domains[X])
     return min(csp.domains[X], key=lambda x: csp.nconflicts(X, x, assignment))
 
-def min_conflicts(csp, assignment={}, max_steps=1000):
+def min_conflicts(csp, max_steps=1000):
     # Solve a CSP by stochastic hillclimbing on the number of conflicts.
     # Generate a complete assignment for all variables (probably with conflicts)
+    assignment={}
     for X in csp.variables:
         val = min_conflicts_value(csp, X, assignment)
         assignment[X] = val
